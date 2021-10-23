7feddfd121db2702be8a8b405e5329b49005d4d8
Ilya Nikolskiy <aspdaspn@gmail.com>
2021-10-23 22:51:16 +0200

    Added GTest library

---
+++ 08/src/main.cpp
@@ -0,0 +1,10 @@
+#include "main.h"
+
+TEST(test_case, example_test) {
+    EXPECT_EQ(2, 2);
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
